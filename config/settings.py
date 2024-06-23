91c65600aca41270928b8938db20d05b25207209
Fayzullo <smartcoderuzb@gmail.com>
2024-06-23 14:11:47 +0000

    ...

--- config/settings.py
+++ config/settings.py
@@ -21,10 +21,9 @@ APPEND_SLASH=False
 CORS_ALLOW_ALL_ORIGINS = True
 
 CORS_ALLOWED_ORIGINS = [
-    'http://example.com',
-    'https://example.com',
-    'http://0.0.0.0:3000',
-    'http://195.158.4.213:3000',
+    'https://zamineducation.uz',
+    'http://10.0.18.27:3000',
+    'http://195.158.4.220:3000',
 ]
 
 # Application definition
